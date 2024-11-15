4de95d36453f7901493ef99ee83bb0b9100dd378
João Marques <joao.marques@levty.com>
2024-11-15 11:35:08 -0300

    Removing process codes file input and changing for text input

--- upload_form.py
+++ upload_form.py
@@ -11,14 +11,6 @@ AWS_S3_BUCKET_NAME = config("AWS_S3_BUCKET_NAME")
 AWS_LAMBDA_NAME = config("AWS_LAMBDA_NAME")
 
 
-def upload_to_s3(file, object_key):
-    try:
-        s3_client.upload_fileobj(file, AWS_S3_BUCKET_NAME, object_key)
-        return True
-    except Exception as e:
-        return False
-    
-
 def invoke_lambda_async(event_payload):    
 
     try:
@@ -33,35 +25,30 @@ def invoke_lambda_async(event_payload):
         print(f"Error invoking Lambda function: {e}")
         return None
 
+
 def run():
     st.title("AutoBMG Processos")
-    st.markdown("Baixe a planilha de exemplo abaixo e preencha com seus dados. Preencha seu email, suas credenciais de acesso do sistema BMG, anexe o arquivo e clique em 'Enviar'.")
-    st.download_button(
-            "Baixar Planilha",
-            data=open("./Template AutoBMG.xlsx", "rb").read(), mime="application/octet-stream", file_name="Template AutoBMG.xlsx"
-        )
+    st.markdown("Preencha seu email, suas credenciais de acesso do sistema do BMG, digite o código do processo e clique em 'Enviar'.")
     with st.form(key='registration_form'):
         email = st.text_input("Email", placeholder="Digite o seu email")
         login = st.text_input("Login", placeholder="Digite o seu login do sistema do BMG")
         password = st.text_input("Senha", type="password", placeholder="Digite sua senha do sistema do BMG")
-        #st.markdown(f'Baixe e preencha a planilha de <a href="https://docs.google.com/spreadsheets/d/1pWRFz9UeFveDPXpiU2T_2zwFYQqOIbUPfBP9JBlBZgg/edit?usp=sharing" target="_blank">exemplo</a> com seus dados. Anexe o arquivo no local abaixo.',  unsafe_allow_html=True)
-        excel_file = st.file_uploader("Planilha", type=["xls", "xlsx"])
+        process_code = st.text_input("Código do processo", placeholder="Digite o código do processo")
         submit_button = st.form_submit_button(label='Enviar')
 
         if submit_button:
-            if login and excel_file is not None:
-                object_key = f"uploads/{uuid4()}.xlsx"
-                if upload_to_s3(excel_file, object_key):
-                    event_payload = {
-                        "email": email,
-                        "login": login,
-                        "password": password,
-                        "file_key": object_key
-                    }
-                    invoke_lambda_async(event_payload)
+            if login and process_code is not None:
+                event_payload = {
+                    "email": email,
+                    "login": login,
+                    "password": password,
+                    "process_code": process_code
+                }
+
+                response = invoke_lambda_async(event_payload)
+                if response is not None:
                     st.success("Formulário enviado com sucesso! Aguarde o retorno dos arquivos no email fornecido.")
                 else:
-                    st.error("Erro ao enviar o formulário. Tente novamente!")
+                    st.warning("Houve um erro ao submeter os dados. Tente novamente.")
             else:
-                st.warning("")
-
+                st.warning("Não foram informados os parâmetros obrigatórios para o download dos arquivos!")
